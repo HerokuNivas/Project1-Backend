964e90c769fa5e1df437ada24b5cb984efa6f761
Nivas <heroku.nivas.deployments@gmail.com>
2022-10-18 16:38:47 +0530

    Printing that api key

--- Index/views.py
+++ Index/views.py
@@ -5,7 +5,9 @@ def HomeRoute(request):
     apiKey = request.GET.get('apikey')
     print("apiKey ", apiKey)
     repoLocation = request.GET.get('location')
+    print("repolcation", repoLocation)
     name = request.GET.get('name')
+    print("name", name)
     message = Code.IR.BSBI(apiKey, repoLocation, name)
     successCode = False
     if message != "Success":
