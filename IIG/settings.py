7106586de25c2d2ce616564260926aeb995fb9c0
Nivas <heroku.nivas.deployments@gmail.com>
2022-10-25 03:43:35 +0530

    Changed settings

--- IIG/settings.py
+++ IIG/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mlkj==u3=+j5l2t43rs4vj3h3yfn)nyqt0hrh8o@_9%cfh*wn='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
