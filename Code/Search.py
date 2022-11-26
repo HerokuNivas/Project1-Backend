15b5afe71597d37284de6154a9d26954aef32e2f
HerokuNivas <heroku.nivas.deployments@gmail.com>
2022-11-26 16:10:51 +0530

    Final Backend I guess

--- Code/Search.py
+++ Code/Search.py
@@ -8,32 +8,36 @@ class Search:
     
     def Search(link, word):
         successCode = "Success"
-        ListFiles = []
-        dictIs = {}
-        lemmatizer = WordNetLemmatizer()
+        if("https://github.com/" not in link or "/blob/main" not in link or len(link.split("/"))!=8):
+            return [{}, "Failure", "", ""]
         link = link.replace("https://github.com/","")
         link = link.replace("/blob/main","")
-        g = Github()
         userName, repoName, fileName = link.split("/")
+        dictIs = {}
         try:
+            ListFiles = []
+            lemmatizer = WordNetLemmatizer()
+            g = Github()
             repo = g.get_repo(f"{userName}/{repoName}")
             files = repo.get_contents("")
+            print(files)
             for file in files:
-                ListFiles.append(file.name) 
+                if(file.name != fileName):
+                    ListFiles.append(file.name)    
             file = repo.get_contents(f"{fileName}")
             content = file.decoded_content.decode()
             line = content.split("\n")
             for singleline in line:
                 words = singleline.split(" ")
-                if lemmatizer.lemmatize(word) in lemmatizer.lemmatize(words[0].lower()):
+                if lemmatizer.lemmatize(word) in lemmatizer.lemmatize(words[0].lower()) and words[0]!='':
                     dictIs[words[0]] = []
-                    for i in range(1,len(words)):
+                    for i in range(3,len(words)-1):
                         if(words[i].isnumeric()):
-                            if(ListFiles[int(words[i])-1] != fileName):
-                                dictIs[words[0]].append(ListFiles[int(words[i])-1])
+                            dictIs[words[0]].append(ListFiles[int(words[i])-1])
                         else:
-                            if(words[i] != fileName):
-                                dictIs[words[0]].append(words[i])      
-        except:
-            successCode = "Failure"                            
+                            dictIs[words[0]].append(words[i])      
+        except Exception as e:
+            print(e)
+            successCode = "Failure"  
+        print(dictIs)                              
         return [dictIs, successCode, userName, repoName]
