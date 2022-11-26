3b10810f771e3d6eb387296c7d7718638bccb6f8
HerokuNivas <heroku.nivas.deployments@gmail.com>
2022-11-26 19:13:05 +0530

    Added

--- Code/Search.py
+++ Code/Search.py
@@ -16,31 +16,31 @@ class Search:
         link = link.replace("/blob/main","")
         userName, repoName, fileName = link.split("/")
         dictIs = {}
-        try:
-            ListFiles = []
-            lemmatizer = WordNetLemmatizer()
-            g = Github(apiKey)
-            repo = g.get_repo(f"{userName}/{repoName}")
-            files = repo.get_contents("")
-            print(files)
-            for file in files:
-                if(file.name != fileName):
-                    ListFiles.append(file.name)    
-            file = repo.get_contents(f"{fileName}")
-            content = file.decoded_content.decode()
-            line = content.split("\n")
-            for singleline in line:
-                words = singleline.split(" ")
-                if lemmatizer.lemmatize(word) in lemmatizer.lemmatize(words[0].lower()) and words[0]!='':
-                    dictIs[words[0]] = []
-                    for i in range(3,len(words)-1):
-                        if(words[i].isnumeric()):
-                            dictIs[words[0]].append(ListFiles[int(words[i])-1])
-                        else:
-                            if(words[i] != fileName):
-                                dictIs[words[0]].append(words[i])      
-        except Exception as e:
-            error = e
-            successCode = "Failure"  
+        # try:
+        ListFiles = []
+        lemmatizer = WordNetLemmatizer()
+        g = Github(apiKey)
+        repo = g.get_repo(f"{userName}/{repoName}")
+        files = repo.get_contents("")
+        print(files)
+        for file in files:
+            if(file.name != fileName):
+                ListFiles.append(file.name)    
+        file = repo.get_contents(f"{fileName}")
+        content = file.decoded_content.decode()
+        line = content.split("\n")
+        for singleline in line:
+            words = singleline.split(" ")
+            if lemmatizer.lemmatize(word) in lemmatizer.lemmatize(words[0].lower()) and words[0]!='':
+                dictIs[words[0]] = []
+                for i in range(3,len(words)-1):
+                    if(words[i].isnumeric()):
+                        dictIs[words[0]].append(ListFiles[int(words[i])-1])
+                    else:
+                        if(words[i] != fileName):
+                            dictIs[words[0]].append(words[i])      
+        # except Exception as e:
+        #     error = e
+        #     successCode = "Failure"  
         print(dictIs)                              
         return [dictIs, successCode, userName, repoName, error]
